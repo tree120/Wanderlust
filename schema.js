2ae17059e3a5392caa8281d2ae7da96a56896139
sweta kumari <144823756+tree120@users.noreply.github.com>
2026-09-09 19:29:04 +0530

    Enhance listingSchema with geometry validation
    Added geometry validation to listing schema.

--- schema.js
+++ schema.js
@@ -1,22 +1,54 @@
-const Joi=require("joi");
 
-module.exports.listingSchema=Joi.object({
-    listing:Joi.object({
-        title:Joi.string().required(),
-        description:Joi.string().required(),
-        category:Joi.string().required(),
-        availability:Joi.string().required(),
-        location:Joi.string().required(),
-        country:Joi.string().required(),
-        price:Joi.number().required().min(0),
-        image:Joi.string().allow("",null),
-    }).required(),
+const Joi = require("joi");
+
+module.exports.listingSchema = Joi.object({
+
+    listing: Joi.object({
+
+        title: Joi.string().required(),
+
+        description: Joi.string().required(),
+
+        category: Joi.string().required(),
+
+        availability: Joi.string().required(),
+
+        location: Joi.string().required(),
+
+        country: Joi.string().required(),
+
+        price: Joi.number().required().min(0),
+
+        image: Joi.string().allow("", null),
+
+        geometry: Joi.object({
+            type: Joi.string()
+                .valid("Point")
+                .required(),
+
+            coordinates: Joi.array()
+                .items(Joi.number())
+                .length(2)
+                .required()
+        }).required()
+
+    }).required()
+
 });
 
-module.exports.reviewSchema=Joi.object({
-    review:Joi.object({
-        rating:Joi.number().required().min(1).max(5),
-        comment:Joi.string().required(),
-    }).required(),
-});
 
+module.exports.reviewSchema = Joi.object({
+
+    review: Joi.object({
+
+        rating: Joi.number()
+            .required()
+            .min(1)
+            .max(5),
+
+        comment: Joi.string()
+            .required()
+
+    }).required()
+
+});
